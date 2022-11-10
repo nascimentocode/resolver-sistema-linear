9c4a3842e83f6dd73aeaa3115da5473ad8ea55f4
João Vitor Nascimento <joaovitornascimento1885@gmail.com>
2022-11-09 22:11:01 -0300

    Trabalho Calculo Numérico

---
+++ main.py
@@ -0,0 +1,54 @@
+# With → Maior aplicação: abertura e fechamento de arquivos
+import csv
+
+import numpy as np
+from numpy.linalg import solve
+
+# def writeCSV(list, pathFile):
+#     with open(pathFile, 'w', newline='') as file:
+#         writer = csv.writer(file, delimiter=',')
+#         for i in list:
+#             writer.writerow(i)
+
+def readCSV(pathFile):
+    valuesCSV = []
+    with open(pathFile, 'r') as file:
+        for line in csv.reader(file, delimiter=","):
+            temp = []
+            for n in line:
+                temp.append(int(n))
+
+            valuesCSV.append(temp)
+
+    return valuesCSV
+
+def solveLinearSystem():
+    system = readCSV("arquivo.csv")
+    lineSystem = len(system)
+    columnSystem = len(system[0])
+    equalities = []
+
+    for i in system:
+        equality = i.pop(-1)
+        equalities.append(equality)
+
+    showInfoLinearSystem(system, lineSystem, columnSystem, equalities)
+
+    result = solve(np.array(system), np.array(equalities))
+    showResult(result)
+
+def showInfoLinearSystem(system, lineSystem, columnSystem, equalities):
+    print("----- Informações do Sistema Linear -----")
+    print(f"Sistema:\n {np.array(system)}")
+    print(f"Igualdades: {np.array(equalities)}")
+    print(f"Número de linhas: {lineSystem}")
+    print(f"Número de colunas: {columnSystem}")
+    print("-----------------------------------------\n")
+def showResult(result):
+    print("Resultado:")
+    for id, x in enumerate(result):
+        print(f"    x{id+1} = {x}")
+
+
+if __name__ == '__main__':
+    solveLinearSystem()
